6fdd6b7eb595d34bc2d8252dbc2916645771438e
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2020-09-10 12:09:56 +0600

    CSV File reading and string opertaion support added

---
+++ Runtime/Others/CSVReader.cs
@@ -0,0 +1,40 @@
+﻿namespace com.faith.core
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    public static class CSVReader
+    {
+        public static int GetNumberOfLineInCSV(TextAsset csvFile)
+        {
+
+            return csvFile.text.Split('\n').Length - 1;
+        }
+
+        public static List<List<string>> GetCSVData(TextAsset csvFile, List<int> colums)
+        {
+
+            string rawCSVText = csvFile.text;
+            string[] csvDataSplitedByNewLine = rawCSVText.Split('\n');
+            int numberOfLineInCSV = csvDataSplitedByNewLine.Length - 1;
+            int numberOfColumn = colums.Count;
+
+            List<List<string>> resultData = new List<List<string>>();
+            for (int i = 0; i < numberOfColumn; i++)
+                resultData[i] = new List<string>();
+
+            for (int i = 0; i < numberOfLineInCSV; i++)
+            {
+
+                string[] csvDataSplitedByComa = csvDataSplitedByNewLine[i].Split(',');
+                for (int j = 0; j < numberOfColumn; j++)
+                {
+
+                    resultData[j].Add(csvDataSplitedByComa[colums[j]]);
+                }
+            }
+
+            return resultData;
+        }
+    }
+}
+
---
+++ Runtime/Others/StringOperation.cs
@@ -0,0 +1,46 @@
+﻿
+namespace com.faith.core
+{
+
+	using System;
+
+    public static class StringOperation
+    {
+		public static string TruncateAllWhiteSpace(string t_ModifiableString)
+		{
+
+			string[] t_SplitByWhiteSpace = t_ModifiableString.Split(' ');
+			string t_NewString = "";
+			foreach (string t_SubString in t_SplitByWhiteSpace)
+			{
+
+				t_NewString += t_SubString;
+			}
+
+			return t_NewString;
+		}
+
+		public static bool IsSameString(string t_String1, string t_String2, bool t_TruncateWhiteSpace = false)
+		{
+			if (t_TruncateWhiteSpace)
+			{
+
+				t_String1 = TruncateAllWhiteSpace(t_String1);
+				t_String2 = TruncateAllWhiteSpace(t_String2);
+			}
+			return String.Equals(t_String1, t_String2);
+		}
+
+		public static bool IsSameStringCI(string t_String1, string t_String2, bool t_TruncateWhiteSpace = false)
+		{
+
+			if (t_TruncateWhiteSpace)
+			{
+				t_String1 = TruncateAllWhiteSpace(t_String1);
+				t_String2 = TruncateAllWhiteSpace(t_String2);
+			}
+			return String.Equals(t_String1, t_String2, StringComparison.OrdinalIgnoreCase);
+		}
+	}
+}
+
