56f6fa25ef0e67f286b515b1f4c14b154ad946d9
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2020-09-13 12:22:54 +0600

    Save load operation deployed

---
+++ Runtime/Others/SaveLoadOperation.cs
@@ -0,0 +1,69 @@
+﻿namespace com.faith.core
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using UnityEngine;
+
+    public class SaveLoadOperation   : MonoBehaviour
+    {
+
+        public static void SaveData<T>(T data, Action OnDataSaved = null, string fileName = "saveFile", string extension = "data") {
+
+            string path = "";
+
+            if (Application.isEditor)
+            {
+                path = Application.dataPath + "/_BinaryFormatedData";
+
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                path += "/" + fileName + "." + extension;
+            }
+            else
+                path = Application.persistentDataPath + "/" + fileName + "." + extension;
+
+            Debug.Log("SavedFile : " + path);
+
+            FileStream fileStream = new FileStream(path, FileMode.Create);
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fileStream, data);
+
+            fileStream.Close();
+
+            OnDataSaved?.Invoke();
+        }
+
+        public static void LoadData<T>(Action OnDataLoadFailed, Action<T> OnDataLoadSucceed = null, string fileName = "saveFile", string extension = "data") {
+
+            T data;
+
+            string path = "";
+
+            if (Application.isEditor)
+                path = Application.dataPath + "/_BinaryFormatedData/" + fileName + "." + extension;
+            else
+                path = Application.persistentDataPath + "/" + fileName + "." + extension;
+
+            if (File.Exists(path))
+            {
+                FileStream fileStream = new FileStream(path, FileMode.Open);
+
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                data = (T) Convert.ChangeType(binaryFormatter.Deserialize(fileStream), typeof(T));
+
+                fileStream.Close();
+
+                OnDataLoadSucceed?.Invoke(data);
+            }
+            else {
+
+                OnDataLoadFailed.Invoke();
+            }
+
+        }
+    }
+}
+
