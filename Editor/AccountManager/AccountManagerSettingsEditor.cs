a572c7abf9a53e86f09c077da04bb5aba37580a3
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2020-09-14 00:35:59 +0600

    Quite amount of work in editor scrip

--- Editor/AccountManager/AccountManagerSettingsEditor.cs
+++ Editor/AccountManager/AccountManagerSettingsEditor.cs
@@ -1,6 +1,8 @@
 ﻿namespace com.faith.core
 {
+    using System.IO;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using UnityEngine;
     using UnityEditor;
 
@@ -12,7 +14,11 @@
         #region Private Variables
         private AccountManagerSettings  _reference;
 
+        private SerializedProperty      _dataSavingMode;
         private SerializedProperty      _listOfCurrencyInfos;
+
+        private static bool _flagedForGeneratingEnum = false;
+        private static List<AccountManagerSettings.CurrecnyInfo> _listOfCurrencyToBeAdded;
 
         #endregion
 
@@ -25,6 +31,10 @@
             if (_reference == null)
                 return;
 
+            if (_listOfCurrencyToBeAdded == null)
+                _listOfCurrencyToBeAdded = new List<AccountManagerSettings.CurrecnyInfo>();
+
+            _dataSavingMode = serializedObject.FindProperty("dataSavingMode");
             _listOfCurrencyInfos = serializedObject.FindProperty("listOfCurrencyInfos");
             if (_reference.listOfCurrencyInfos == null) {
 
@@ -37,7 +47,77 @@
         {
             serializedObject.Update();
 
+            EditorGUILayout.BeginHorizontal();
+            {
+                EditorGUILayout.PropertyField(_dataSavingMode);
+                if (GUILayout.Button("+Add", GUILayout.Width(50f))){
+
+                    AddNewCurrency();
+                }
+                if (_flagedForGeneratingEnum && GUILayout.Button("GenerateEnum", GUILayout.Width(125))) {
+
+                    _flagedForGeneratingEnum = false;
+                    GenerateEnum();
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+
             serializedObject.ApplyModifiedProperties();
+        }
+
+        #endregion
+
+        #region Configuretion
+
+        private void AddNewCurrency() {
+
+            _listOfCurrencyToBeAdded.Add(new AccountManagerSettings.CurrecnyInfo());
+            _flagedForGeneratingEnum = true;
+        }
+
+        private async void GenerateEnum() {
+
+            string path = Application.dataPath + "/AccountManagerEnums";
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            path += "/AccountManagerCurrencyEnum.cs";
+
+            using (StreamWriter streamWriter = new StreamWriter(path)) {
+
+                string scriptData = "namespace com.faith.core";
+                scriptData += "\n{";
+                scriptData += "\n\tpublic enum AccountManagerCurrencyEnum";
+                scriptData += "\n\t{";
+                //Starting
+
+                foreach (AccountManagerSettings.CurrecnyInfo currencyInfo in _reference.listOfCurrencyInfos)
+                    scriptData += "\n\t\t" + currencyInfo.currencyName.ToUpper();
+
+                foreach (AccountManagerSettings.CurrecnyInfo currencyInfo in _listOfCurrencyToBeAdded)
+                    scriptData += "\n\t\t" + currencyInfo.currencyName.ToUpper();
+
+                //Ending
+                scriptData += "\n\t}";
+                scriptData += "\n}";
+
+                await streamWriter.WriteAsync(scriptData);
+            }
+
+            AssetDatabase.Refresh();
+            await Task.Delay(500);
+            while (EditorApplication.isCompiling)
+                await Task.Delay(100);
+
+            int numberOfCurrencyToBeAdded = _listOfCurrencyToBeAdded.Count;
+            for (int i = 0; i < numberOfCurrencyToBeAdded; i++) {
+
+                _reference.listOfCurrencyInfos.Add(_listOfCurrencyToBeAdded[0]);
+                _listOfCurrencyToBeAdded.RemoveAt(0);
+            }
+
+            _listOfCurrencyInfos.serializedObject.ApplyModifiedProperties();
         }
 
         #endregion
