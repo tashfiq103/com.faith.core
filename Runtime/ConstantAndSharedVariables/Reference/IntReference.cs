46385932ff0a360ea7db674a24f13c2593e528cc
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2020-10-15 12:09:27 +0600

    Float Variable and IntVariable is now included in core package

---
+++ Runtime/ConstantAndSharedVariables/Reference/IntReference.cs
@@ -0,0 +1,30 @@
+﻿namespace com.faith.core
+{
+    [System.Serializable]
+    public class IntReference
+    {
+        public bool UseConstant = true;
+        public int ConstantValue;
+        public IntVariables Variable;
+
+        public IntReference()
+        { }
+
+        public IntReference(int value)
+        {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+        public int Value
+        {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+
+        public static implicit operator int(IntReference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
+
---
+++ Runtime/ConstantAndSharedVariables/Variables/IntVariables.cs
@@ -0,0 +1,35 @@
+﻿namespace com.faith.core
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "IntVariables", menuName = "FAITH/SharedVariable/IntVariables")]
+    public class IntVariables : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+        public int Value;
+
+        public void SetValue(int value)
+        {
+            Value = value;
+        }
+
+        public void SetValue(IntVariables value)
+        {
+            Value = value.Value;
+        }
+
+        public void ApplyChange(int amount)
+        {
+            Value += amount;
+        }
+
+        public void ApplyChange(IntVariables amount)
+        {
+            Value += amount.Value;
+        }
+    }
+}
+
