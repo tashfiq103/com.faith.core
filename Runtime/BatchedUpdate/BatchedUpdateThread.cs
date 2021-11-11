0c33c489037477b8431668bad32615913ced53da
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2021-11-11 12:07:05 +0600

    IsUpdateThreadRunning variable is available for BatchedUpdateThread

--- Runtime/BatchedUpdate/BatchedUpdateThread.cs
+++ Runtime/BatchedUpdate/BatchedUpdateThread.cs
@@ -3,6 +3,7 @@ namespace com.faith.core
     using UnityEngine.Events;
     public class BatchedUpdateThread : IBatchedUpdateHandler
     {
+        public bool IsUpdateThreadRunning { get; private set; } = false;
         public UnityAction Update;
 
         public BatchedUpdateThread(UnityAction Update)
@@ -13,10 +14,12 @@ namespace com.faith.core
         public void StartUpdate(int batchInterval = 1)
         {
             BatchedUpdate.Instance.RegisterToBatchedUpdate(this, batchInterval);
+            IsUpdateThreadRunning = true;
         }
 
         public void StopUpdate()
         {
+            IsUpdateThreadRunning = false;
             BatchedUpdate.Instance.UnregisterFromBatchedUpdate(this);
         }
 
